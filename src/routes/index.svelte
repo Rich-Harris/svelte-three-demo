96ccdbff4f7311bb0a855679a6e9c09edeed699e
Rich Harris <richard.a.harris@gmail.com>
2021-09-28 17:13:51 -0400

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+	import * as THREE from 'three';
+	import * as GL from 'svelte-three';
+</script>
+
+<GL.Canvas>
+	<GL.Mesh geometry={new THREE.BoxGeometry()}/>
+	<GL.PerspectiveCamera position={[2, 2, 5]}/>
+</GL.Canvas>
